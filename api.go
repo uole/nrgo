8925a65fd72e1da1de41ca1111095ab648fcfc5a
fancl <fancl@justcall.cn>
2023-07-13 14:57:02 +0800

    add version api

--- api.go
+++ api.go
@@ -3,6 +3,7 @@ package nrgo
 import (
 	"git.nspix.com/golang/kos"
 	"git.nspix.com/golang/kos/entry/http"
+	"github.com/uole/nrgo/version"
 )
 
 func (svr *Server) handleIndexRoute(ctx *http.Context) (err error) {
@@ -17,10 +18,18 @@ func (svr *Server) handleListSessions(ctx *http.Context) (err error) {
 	return ctx.Success(svr.getSessionSnapshot())
 }
 
+func (svr *Server) handleShowVersion(ctx *http.Context) (err error) {
+	return ctx.Success(map[string]any{
+		"version":   version.Version,
+		"buildDate": version.BuildDate,
+	})
+}
+
 func (svr *Server) routes() {
 	kos.Http().Handle(http.MethodGet, "/", svr.handleIndexRoute)
 	kos.Http().Group("/api/v1", []http.Route{
 		{http.MethodGet, "/info", svr.handleNodeInfo},
+		{http.MethodGet, "/version", svr.handleShowVersion},
 		{http.MethodGet, "/sessions", svr.handleListSessions},
 	})
 }
--- version/version.go
+++ version/version.go
@@ -3,4 +3,5 @@ package version
 var (
 	Version     = "0.0.1"
 	ProductName = "nrgo"
+	BuildDate   = ""
 )
